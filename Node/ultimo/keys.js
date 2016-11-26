059817ed9c10c100c92d538ba2af9e69d6e8396a
EC2 Default User <ec2-user@ip-172-31-1-252.us-west-1.compute.internal>
2016-11-26 14:55:57 +0000

    new_keys

--- Node/ultimo/keys.js
+++ Node/ultimo/keys.js
@@ -2,7 +2,7 @@
  * Created by manatee on 26.11.16.
  */
 module.exports = {
-    CALLBACK_URL: "https://42002a77.ngrok.io/api/calls",
+    CALLBACK_URL: "https://6cc0a374.ngrok.io/api/calls",
     MICROSOFT_APP_ID: "e6af5bf0-0a0c-491c-b445-0329bffef266",
     MICROSOFT_APP_PASSWORD: "YiPpmDpEPDqZ120Pcpvse5A"
 };
